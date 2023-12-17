c50369b807c2a05015663d27dc78e7bbf492bdc0
raihanmd <mraihanf471@gmail.com>
2023-12-17 18:59:12 +0700

    change compiler to turbo and create master_mapel table

--- prisma/migrations/20231217115738_create/migration.sql
+++ prisma/migrations/20231217115738_create/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
     `id` VARCHAR(14) NOT NULL,
     `username` VARCHAR(50) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
@@ -10,6 +10,6 @@ CREATE TABLE `Users` (
     `createdAt` INTEGER NOT NULL,
     `lastLogin` INTEGER NULL,
 
-    UNIQUE INDEX `Users_username_key`(`username`),
+    UNIQUE INDEX `users_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
