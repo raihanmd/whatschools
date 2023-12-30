cf7b43a7c458d4aa762da4f2188201b954e739a6
raihanmd <mraihanf471@gmail.com>
2023-12-30 12:50:21 +0700

    feat form /ppdb/daftar

--- src/app/ppdb/daftar/components/RegistrationForm/index.tsx
+++ src/app/ppdb/daftar/components/RegistrationForm/index.tsx
@@ -2,12 +2,12 @@
 
 // @ts-ignore
 import { useSteps } from "chakra-ui-steps";
-import { Button, Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
 
 import type { Step } from "./types";
 import FormStepper from "./FormStepper";
 import color from "@/config/color";
-import InputForm from "./InputForm";
+import InputField from "./InputField";
 import ConfirmationForm from "./ConfirmationForm";
 
 export default function RegistrationForm() {
@@ -16,15 +16,29 @@ export default function RegistrationForm() {
   });
 
   return (
-    <Stack p={"3"} bg={color.light.foreground}>
+    <Stack p={"5"} bg={color.light.foreground}>
       <FormStepper activeStep={activeStep} step={STEPS} />
-      {activeStep === 0 ? <InputForm /> : <ConfirmationForm />}
-      <Button isDisabled={activeStep === 0} onClick={() => prevStep()}>
-        Prev
-      </Button>
-      <Button isDisabled={activeStep === 2} onClick={() => nextStep()}>
-        Next
-      </Button>
+      {activeStep === 0 ? <InputField /> : <ConfirmationForm />}
+      <Flex justify={"end"} align={"center"} gap={"3"}>
+        <Button
+          isDisabled={activeStep === 0}
+          bg={color.utility.error.background}
+          color={color.utility.error.content}
+          _hover={{ bg: color.utility.error.background }}
+          onClick={() => prevStep()}
+        >
+          Prev
+        </Button>
+        <Button
+          isDisabled={activeStep === 2}
+          bg={color.utility.success.background}
+          color={color.utility.success.content}
+          _hover={{ bg: color.utility.success.background }}
+          onClick={() => nextStep()}
+        >
+          Next
+        </Button>
+      </Flex>
     </Stack>
   );
 }
--- src/app/ppdb/daftar/components/RegistrationForm/types/index.ts
+++ src/app/ppdb/daftar/components/RegistrationForm/types/index.ts
@@ -7,3 +7,17 @@ export type Step = {
   label: string;
   description: string;
 };
+
+type InputField = {
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  name?: string;
+  options?: Array<string>;
+  description?: string;
+};
+
+export type InputFields = InputField & {
+  isMerged?: boolean;
+  fields?: Array<InputField>;
+};
